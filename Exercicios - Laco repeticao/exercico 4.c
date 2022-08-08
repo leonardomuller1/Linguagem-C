3cb0ee0dc72dbed5f66be084faefb7504ea6f964
leonardomuller1 <81366887+leonardomuller1@users.noreply.github.com>
2022-08-08 13:49:45 -0300

    Update exercico 4.c

--- Exercicios - Laco repeticao/exercico 4.c
+++ Exercicios - Laco repeticao/exercico 4.c
@@ -11,21 +11,41 @@ Questao:
  Modifique o programa do exercício anterior de forma a exibir XX no lugar dos valores múltiplos de 6.
 */
 
-// Includes
-#include <stdio.h>
-#include <conio.h>
+/*
+ * @brief   numero primo
+ * @author  Leonardo Muller
+ *
+ * @version 0.1
+ * @date    jul/2022
+ */
 
-int main()
-{
-    int i;
-    
-for (i=1; i <=60; i++){
-    if(i==11||i==21||i==31||i==41||i==51){
-        printf("\n");
-    }else if(i%6==0){
-        printf("XX ");
-    }else{
-          printf("%2d ",i);  
+/*
+Questao:
+Calcule se o valor fornecido pelo usuário é um número primo. (Dica : construa uma função para identificar números primos)..
+*/
+
+// Includes
+#include<stdio.h>
+#include <locale.h>
+ 
+int main() {
+ setlocale(LC_ALL, "");
+ int num, i, resultado = 0;
+ 
+ printf("Digite um número: ");
+ scanf("%d", &num);
+ 
+ for (i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+       resultado++;
+       break;
     }
-}
+ }
+ 
+ if (resultado == 0)
+    printf("%d é um número primo\n", num);
+ else
+    printf("%d não é um número primo\n", num);
+ 
+ return 0;
 }
