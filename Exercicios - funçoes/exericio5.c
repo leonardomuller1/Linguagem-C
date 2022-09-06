11468bbf3e383a162abcbffc01abecfd13b31e56
leonardomuller1 <81366887+leonardomuller1@users.noreply.github.com>
2022-09-06 13:59:50 -0300

    sinal para frequencia

---
+++ Exercicios - funçoes/exericio5.c
@@ -0,0 +1,42 @@
+/*
+ * @brief   conversor de sinal para frequencia
+ * @author  Leonardo Muller
+ *
+ * @version 0.1
+ * @date    set/2022
+ */
+
+/*
+Questao:
+Calcule o período de um sinal a partir da sua frequência
+Parametros : float
+Retorno : float
+*/
+#include <stdio.h>
+#include <stdlib.h>
+//prototipo
+float SinalParaFrequencia (float sinal);
+
+int main(){
+
+float sinal;
+float sinaloriginal;
+do{
+    fflush(stdin);
+    printf("Entre com o sinal:");
+    scanf("%f",&sinal);
+    printf("\n");
+
+    sinaloriginal = sinal;
+    sinal = SinalParaFrequencia (sinal);
+    printf("O sinal %.2f tem uma frequencia %f HZ\n\n",sinaloriginal,sinal);
+
+}while(1);
+
+}
+
+float SinalParaFrequencia(float sinal){
+    float frequencia;
+    frequencia = 1/sinal; // F = 1/ T
+    return frequencia;
+}
