71fa21478bd6fa88efa751be533da485406ed505
leonardomuller1 <81366887+leonardomuller1@users.noreply.github.com>
2022-10-17 21:04:13 -0300

    Add files via upload

---
+++ exercicios - strings/exercicio5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    
+char nome[50];
+char sobrenome[50];
+char nomeCompleto[100];
+int i;
+int j;
+printf("Digite seu nome:");
+scanf("%s",nome);
+
+printf("Digite seu sobrenome:");
+scanf("%s",sobrenome);
+
+
+i=0;
+do{
+    nomeCompleto[i]=nome[i];
+
+}while(nome[i++]!='\0');
+i--;
+
+nomeCompleto[i++]=' ';
+
+j=0;
+do{
+    nomeCompleto[i++]=sobrenome[j];
+
+}while(sobrenome[j++]!='\0');
+
+printf("Bem vindo, %s\n",nomeCompleto);
+
+
+}
