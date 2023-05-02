17d18f7066e7f3670d2c33087b2d3e524d28a208
Leonardo Muller <81366887+leonardomuller1@users.noreply.github.com>
2023-05-02 17:56:30 -0300

    Create ex4.c

---
+++ 3 ano/exercicios/ponteiros/ex4.c
@@ -0,0 +1,37 @@
+/*
+4. int sinal(float *frequencia, float *periodo );
+Função : Calcula o parâmetro cujo valor é zero.
+Parâmetros : frequencia – ponteiro para o valor de frequencia do sinal
+             periodo – ponteiro para o valor do periodo do sinal
+
+Retorno : 0 para função realizada, -1 para erro de parametro (dois parametros
+em zero)
+*/
+#include <stdio.h>
+
+int sinal(float *frequencia, float *periodo );
+
+int main() {
+    float frequencia, periodo;
+
+    printf("Qual a frequencia?");
+    scanf("%f",&frequencia);
+
+    printf("Qual a periodo?");
+    scanf("%f",&periodo);
+
+    printf("%i",sinal(&frequencia,&periodo));
+
+}
+
+int sinal(float *frequencia, float *periodo ) {
+   if(*frequencia == 0 && *periodo == 0){
+        return -1;
+   }else if(*frequencia && *periodo){
+           return -2;
+   }else if(*frequencia == 0){
+        return 0;
+   }else if(*periodo == 0){
+        return 0;
+   }
+}
